643b11516d7e87a4322c7b7ccc70fb648a55139e
MaximMarinov <maximmarinovv@gmail.com>
2022-04-13 04:19:00 +0300

    Starting all over -> Creating the backend

---
+++ backend/models/Item.js
@@ -0,0 +1,27 @@
+const { model, Schema, Types: { ObjectId } } = require('mongoose');
+
+const schema = new Schema({
+    title: { type: String, required: true },
+    img: { type: String, required: true },
+    year: {
+        type: Number,
+        required: true,
+        min: [1950, 'Year must be between 1950 and 2022'],
+        max: [2022, 'Year must be between 1950 and 2022']
+    },
+    engine: { type: String, required : true },
+    transmission: { type: String, required : true },
+    place: { type: String, required : true },
+    cubature: { type: Number, required : true },
+    mileage: { type: Number, required : true },
+    category: { type: String, required : true },
+    eurostandard: { type: Number, required : true },
+    color: { type: String, required : true },
+    description: { type: String, required: true, minlength: [10, 'Description must be at least 10 characters long'] },
+    price: { type: Number, required: true },
+    owner: { type: ObjectId, ref: 'User' }
+});
+
+const Item = model('Item', schema);
+
+module.exports = Item;
---
+++ backend/services/ads.js
@@ -0,0 +1,51 @@
+const Item = require('../models/Item');
+
+
+async function getAll() {
+    return Item.find({});
+}
+
+async function create(item) {
+    const result = new Item(item);
+    await result.save();
+
+    return result;
+}
+
+function getById(id) {
+    return Item.findById(id);
+}
+
+async function update(id, item) {
+    const existing = await Item.findById(id);
+
+    existing.title = item.title;
+    existing.img = item.img;
+    existing.year = item.year;
+    existing.engine = item.engine;
+    existing.transmission = item.transmission;
+    existing.place = item.place;
+    existing.cubature = item.cubature;
+    existing.mileage = item.mileage;
+    existing.eurostandard = item.eurostandard;
+    existing.color = item.color;
+    existing.description = item.description;
+    existing.price = item.price;
+
+    await existing.save();
+
+    return existing;
+}
+
+async function deleteById(id) {
+    await Item.findByIdAndDelete(id);
+}
+
+
+module.exports = {
+    getAll,
+    create,
+    getById,
+    update,
+    deleteById
+};
