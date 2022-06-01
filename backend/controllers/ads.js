bc4cc6cd94011b7fdf62a1ebb38d0c07d24f6b3f
MaximMarinov <maximmarinovv@gmail.com>
2022-06-01 18:11:22 +0300

    Added server side upload

--- backend/controllers/ads.js
+++ backend/controllers/ads.js
@@ -4,7 +4,29 @@ const { isAuth, isOwner } = require('../middlewares/guards');
 const mapErrors = require('../utils/mapper');
 const preload = require('../middlewares/preload');
 const User = require('../models/User');
+const multer = require("multer");
 
+const MIME_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpg',
+    'image/jpg': 'jpg'
+};
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const isValid = MIME_TYPE_MAP[file.mimetype];
+        let error = new Error('Invalid mime type');
+        if (isValid) {
+            error = null;
+        }
+        cb(error, "backend/images");
+    },
+    filename: (req, file, cb) => {
+        const name = file.originalname.toLowerCase().split(' ').join('-');
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        cb(null, name + '-' + Date.now() + '.' + ext); 
+    }
+});
 
 router.get('/', async (req, res) => {
     console.log(req.user);
@@ -12,7 +34,7 @@ router.get('/', async (req, res) => {
     res.json(data);
 });
 
-router.post('/', isAuth(), async (req, res) => {
+router.post('/', multer(storage).single('img'), isAuth(), async (req, res) => {
     const item = {
         title: req.body.title,
         img: req.body.img,
